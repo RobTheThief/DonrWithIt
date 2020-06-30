ee8bfa6215fba1b089083567254533585ea6ebb3
RobTheThief <cyberpeaches@gmail.com>
2020-06-30 15:06:05 +0100

    Created expo project and layout for main screens

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function App() {
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
+import Cards from "./app/components/Cards";
+
+export default function app() {
+  console.log("App Running!!");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Cards
+        title="Red Jacket for Sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
     </View>
+
+    //<ViewImageScreen />; <AppText>I love React!!</AppText> <MaterialCommunityIcons name="email" size={200} color="tomato" /> <WelcomeScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      blurRadius={3}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo-red.png")} style={styles.logo} />
+        <Text style={styles.tagline}>Sell What you Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
+});
+export default WelcomeScreen;
+
+/*
+
+<ImageBackground
+      source={require("./app/assets/background.jpg")}
+      
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 20,
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={require("./app/assets/logo-red.png")}
+          style={{
+            width: 100,
+            height: 100,
+          }}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#fc5c65",
+          width: "100%",
+          height: 60,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "#4ECDC4",
+          width: "100%",
+          height: 60,
+        }}
+      />
+    </ImageBackground>
+    */
